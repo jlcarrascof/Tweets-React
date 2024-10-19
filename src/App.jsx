ef3e8858f80338362b43bffcb5755ba26bf25173
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-18 20:32:12 -0400

    Refactor App component to include TweetBox component and update heading text

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import './App.css'
+import TweetBox from './components/TweetBox.jsx';
 
-import TweetBox from './components/TweetBox';
-
-function App() {
+export default function App() {
   return (
     <div className="p-4">
       <h1 className="text-3xl font-bold text-center">Clon de Twitter/X</h1>
       <TweetBox />
     </div>
-  );
+  )
 }
-
-export default App;
