5ccb1866858a9f32a56de428e8b920f069ab0399
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-18 21:09:16 -0400

    Refactor package name and remove unnecessary files

--- src/components/TweetBox.jsx
+++ src/components/TweetBox.jsx
@@ -1,13 +1,13 @@
 export default function TweetBox() {
-  return (
-    <div className="border p-4 rounded-lg shadow-md max-w-md mx-auto mt-4">
-      <textarea
-        className="w-full border p-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-        placeholder="¿What's happening?"
-      ></textarea>
-      <button className="bg-blue-500 text-white py-2 px-4 rounded-md mt-2 hover:bg-blue-600">
-        Send Tweet
-      </button>
-    </div>
-  );
+    return (
+      <div className="border p-4 rounded-lg shadow-md max-w-md mx-auto mt-4">
+        <textarea
+          className="w-full border p-2 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+          placeholder="¿What's happening?"
+        ></textarea>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded-md mt-2 hover:bg-blue-600">
+          Send Tweet
+        </button>
+      </div>
+    );
 }
