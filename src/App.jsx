c37f753745178225ff39cf7d8c8f9b7a6c6ba19d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-23 12:31:19 -0400

    Refactor App component to update header styling and add margin to the main content

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import TweetBox from './components/TweetBox'
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
-      <h1 className='text-3xl font-bold text-center'>Twitter/X Clone</h1>
-      <TweetBox />
+      <header className="bg-blue-500 text-white flex justify-between items-center p-4 fixed top-0 left-0 w-full z-10">
+        <h1 className='text-3xl font-bold text-center'>Twitter/X Clone</h1>
+        <TweetBox />
+      </header>
     </div>
   )
 }
