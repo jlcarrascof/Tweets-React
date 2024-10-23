44b05dbb0b672ecd111e7b19a0388b611e22b9d4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-23 11:21:03 -0400

    Refactor App component to update header styling, add margin to the main content, and remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import './App.css'
-import TweetBox from './components/TweetBox.jsx';
+import TweetBox from './components/TweetBox'
 
 export default function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <header className="bg-blue-500 text-white text-center p-4 fixed top-0 left-0 w-full z-10">
-        <h1 className="text-3xl font-bold">Twitter/X Clone</h1>
-      </header>
-      <div className="pt-10 flex-1 flex flex-col items-center mt-4">
-        <TweetBox />
-      </div>
-      <footer className="bg-gray-800 text-white text-center p-4 fixed bottom-0 left-0 w-full">
-        <p>&copy; 2024 Twitter/X Clone. All rights reserved.</p>
-      </footer>
+    <div className='p-4'>
+      <h1 className='text-3xl font-bold text-center'>Twitter/X Clone</h1>
+      <TweetBox />
     </div>
   )
 }
