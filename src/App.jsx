3e4b471b66aab6e9bbeb34aee472c776648d8118
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-23 12:39:18 -0400

    Refactor App component: Update header styling and add user icons

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,9 @@ export default function App() {
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-500 text-white flex justify-between items-center p-4 fixed top-0 left-0 w-full z-10">
         <h1 className="text-3xl font-bold pl-8">Clon de Twitter/X</h1>
+        <div className="flex items-center space-x-4 pr-8">
 
+        </div>
       </header>
       <TweetBox />
 
