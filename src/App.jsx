86b476a7a9ed5601c2029e119568d97225b5f1af
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-23 12:38:35 -0400

    Refactor App component: Update header styling and add TweetBox component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,10 @@ export default function App() {
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-500 text-white flex justify-between items-center p-4 fixed top-0 left-0 w-full z-10">
         <h1 className="text-3xl font-bold pl-8">Clon de Twitter/X</h1>
-        <TweetBox />
+
       </header>
+      <TweetBox />
+
     </div>
   )
 }
