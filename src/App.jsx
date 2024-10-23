b2e312638a065f8ec4e06562ecc236e6dfba1f85
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-22 22:59:54 -0400

    Refactor App component to update header styling and add fixed position

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import TweetBox from './components/TweetBox.jsx';
 
 export default function App() {
   return (
-    <div className="p-4">
-      <header className="bg-blue-500 text-white text-center p-4">
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-blue-500 text-white text-center p-4 fixed top-0 left-0 w-full z-10">
         <h1 className="text-3xl font-bold text-center">Twitter/X Clone</h1>
       </header>
       <TweetBox />
