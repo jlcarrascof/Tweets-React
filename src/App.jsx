b6f202aebe8da2ef6aad9e3c6f1b042fa836607b
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-23 12:42:57 -0400

    Refactor App component: Add register button with user plus icon

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,12 @@ export default function App() {
         <h1 className="text-3xl font-bold pl-8">Clon de Twitter/X</h1>
         <div className="flex items-center space-x-4 pr-8">
           <button className="flex items-center bg-white text-blue-500 py-1 px-4 rounded hover:bg-gray-100">
-          <UserIcon className="h-5 w-5 mr-2" />
-            Login
+            <UserIcon className="h-5 w-5 mr-2" />
+              Login
+          </button>
+          <button className="flex items-center bg-white text-blue-500 py-1 px-4 rounded hover:bg-gray-100">
+            <UserPlusIcon className="h-5 w-5 mr-2" />
+              Register
           </button>
         </div>
       </header>
