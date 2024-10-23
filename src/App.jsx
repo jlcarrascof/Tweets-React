e34f2a53f3c5cb2d866ae9a74aed3379df9b81be
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-23 12:36:32 -0400

    Refactor App component to update header styling, import UserPlusIcon, and translate header text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { UserIcon, UserAddIcon } from '@heroicons/react/solid';
+import { UserIcon, UserPlusIcon } from '@heroicons/react/24/solid';
 import './App.css'
 import TweetBox from './components/TweetBox'
 
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-500 text-white flex justify-between items-center p-4 fixed top-0 left-0 w-full z-10">
-        <h1 className='text-3xl font-bold text-center'>Twitter/X Clone</h1>
+        <h1 className="text-3xl font-bold pl-8">Clon de Twitter/X</h1>
         <TweetBox />
       </header>
     </div>
