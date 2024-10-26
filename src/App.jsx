f620210d21cfb3591257c3da8133970be22cd20e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-25 22:38:42 -0400

    Refactor App component: Add retweet functionality

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,12 @@ export default function App() {
     setTweets(updatedTweets);
   }
 
+  const handleRetweet = (index) => {
+    const updatedTweets = [...tweets];
+    updatedTweets[index].retweets += 1;
+    setTweets(updatedTweets);
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <header className="bg-blue-500 text-white flex justify-between items-center p-4 fixed top-0 left-0 w-full z-10">
