55258f3204f5982a66decdd2bc393aa65fe6cb5e
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-28 19:09:49 -0400

    Refactor Tweet component: Add tweet header with user and date/time

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,6 +2,13 @@ export default function Tweet({ user, body, date, time, likes, retweets, onLike,
 
     <div className="border p-4 rounded-lg bg-white shadow-md">
 
+         {/* header of tweet */}
+
+        <div className="flex justify-between items-center border-b pb-2 mb-2">
+            <span className="font-bold text-gray-800">@{user}</span>
+            <span className="text-sm text-gray-500">{date} - {time}</span>
+        </div>
+
     </div>
 
 }
