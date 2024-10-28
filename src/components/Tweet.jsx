08e0d3ee8341df2e8591a8db6084d64bbee57100
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-28 19:21:17 -0400

    Refactor Tweet component: Add missing div container for tweet body

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -2,11 +2,17 @@ export default function Tweet({ user, body, date, time, likes, retweets, onLike,
 
     <div className="border p-4 rounded-lg bg-white shadow-md">
 
-         {/* header of tweet */}
+        {/* header of tweet */}
 
         <div className="flex justify-between items-center border-b pb-2 mb-2">
             <span className="font-bold text-gray-800">@{user}</span>
             <span className="text-sm text-gray-500">{date} - {time}</span>
+        </div>
+
+        {/* body of tweet */}
+
+        <div className="text-gray-700 text-base mb-4 border-b pb-4">
+            {body}
         </div>
 
     </div>
