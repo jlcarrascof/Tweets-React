f315680a9ff9e4959eb8082a91670fd207ff99e5
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-28 19:06:52 -0400

    Refactor Tweet component: Add missing div container for tweet display

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,7 +1,7 @@
 export default function Tweet({ user, body, date, time, likes, retweets, onLike, onRetweet }) {
 
+    <div className="border p-4 rounded-lg bg-white shadow-md">
 
-
-
+    </div>
 
 }
