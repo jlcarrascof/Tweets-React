0d507ae0aaadbb4b482a7f3f793b7818db331e71
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-30 09:59:32 -0400

    Refactor App.jsx: Remove commented out code for login and register buttons

--- src/App.jsx
+++ src/App.jsx
@@ -57,8 +57,6 @@ export default function App() {
             )
           }
 
-          {showModal && <AuthModal onClose={() => setShowModal(false)} onRegister={handleRegister} />}
-
           {/*
           <button className="flex items-center bg-white text-blue-500 py-1 px-4 rounded hover:bg-gray-100">
             <UserIcon className="h-5 w-5 mr-2" />
@@ -72,6 +70,8 @@ export default function App() {
 
         </div>
       </header>
+
+      {showModal && <AuthModal onClose={() => setShowModal(false)} onRegister={handleRegister} />}
 
       <div className="pt-10 flex-1 flex flex-col items-center mt-4">
         <TweetBox onTweetSubmit={addTweet} tweetCount={tweets.length} />
