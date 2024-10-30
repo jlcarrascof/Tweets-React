e7bcec541e192c2a5619626a470aebd3b3189fc9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-10-30 09:23:04 -0400

    Refactor App.jsx: Remove commented out code for login and register buttons

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,10 @@ export default function App() {
       <header className="bg-blue-500 text-white flex justify-between items-center p-4 fixed top-0 left-0 w-full z-10">
         <h1 className="text-3xl font-bold pl-8">Twitter/X Clone</h1>
         <div className="flex items-center space-x-4 pr-8">
+
+
+
+          {/*
           <button className="flex items-center bg-white text-blue-500 py-1 px-4 rounded hover:bg-gray-100">
             <UserIcon className="h-5 w-5 mr-2" />
               Login
@@ -45,6 +49,8 @@ export default function App() {
             <UserPlusIcon className="h-5 w-5 mr-2" />
               Register
           </button>
+          */}
+
         </div>
       </header>
 
