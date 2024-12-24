a6c112d77fe7b4ab156e90034747161419ed23f3
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-12-24 11:13:44 -0400

    Refactor App.jsx: Add user prop to TweetBox component for enhanced functionality

--- src/App.jsx
+++ src/App.jsx
@@ -94,7 +94,7 @@ export default function App() {
       )}
 
       <div className="pt-10 flex-1 flex flex-col items-center mt-4">
-        <TweetBox onTweetSubmit={addTweet} tweetCount={tweets.length} />
+        <TweetBox onTweetSubmit={addTweet} tweetCount={tweets.length} user  = {user} />
 
         <div className="mt-8 w-full max-w-md space-y-4">
           {tweets.map((tweet, index) => (
